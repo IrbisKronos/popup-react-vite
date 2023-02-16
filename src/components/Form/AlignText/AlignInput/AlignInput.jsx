03a49a2d8401635b5ca103d4640ec4231463b423
Artem Vasylenko <irbiskronos@gmail.com>
2023-02-16 12:57:48 +0200

    Input radio problem

--- src/components/Form/AlignText/AlignInput/AlignInput.jsx
+++ src/components/Form/AlignText/AlignInput/AlignInput.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './AlignInput.css';
 
-export default function AlignInput({ id, icon, className, ...rest }) {
+export default function AlignInput({ value, id, icon, className, ...rest }) {
   return (
     <>
-      <input id={id} {...rest} />
+      <input type='radio' name='alignText' value={value} id={id} {...rest} />
       <label htmlFor={id} className={className}>
         <span className='material-symbols-outlined'>{icon}</span>
       </label>
--- src/components/Form/AlignText/AlignText.jsx
+++ src/components/Form/AlignText/AlignText.jsx
@@ -9,47 +9,42 @@ export default function AlignText({ context, DataBound }) {
     <div className='segmentedControl' id='align_text_select'>
       <DataBoundAlignInput
         debug
-        type='radio'
-        name='alignText'
         value='left'
         id='headerLeft'
         icon='format_align_left'
         className='segmentedControl1'
         context={context}
-        defaultChecked
+        defaultChecked={context.alignText === 'left'}
       />
 
       <DataBoundAlignInput
         debug
-        type='radio'
-        name='alignText'
         value='center'
         id='headerCenter'
         icon='format_align_center'
         className='segmentedControl2'
         context={context}
+        defaultChecked={context.alignText === 'center'}
       />
 
       <DataBoundAlignInput
         debug
-        type='radio'
-        name='alignText'
         value='right'
         id='headerRight'
         icon='format_align_right'
         className='segmentedControl3'
         context={context}
+        defaultChecked={context.alignText === 'right'}
       />
 
       <DataBoundAlignInput
         debug
-        type='radio'
-        name='alignText'
         value='justify'
         id='headerJustify'
         icon='format_align_justify'
         className='segmentedControl4'
         context={context}
+        defaultChecked={context.alignText === 'justify'}
       />
 
       <div className='segmentedControlColor'></div>
